f93cbc1ea03c260fca23c4e725c930c3b75c741d
Izak002 <izaklerouxx02@gmail.com>
2025-01-30 14:34:24 +0200

    Install gems in project ✨

--- run.sh
+++ run.sh
@@ -2,3 +2,12 @@
 
 # script for running my ruby file [im just lazy 🌙✨]
 
+# Install bundler if not exists
+if ! gem list bundler -i > /dev/null; then
+  echo "Bundler was not found installing"
+  gem install bundler
+fi
+
+# run script
+bundle install
+bundle exec ruby lumen.rb
